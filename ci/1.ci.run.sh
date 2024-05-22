8ce48315a9c46adfe5410cf79d4ef31037bb4bd3
gnuhub <gnuhub@qq.com>
2024-05-22 08:51:13 +0800

    [just fix it]

--- ci/1.ci.run.sh
+++ ci/1.ci.run.sh
@@ -4,4 +4,12 @@ set -x
 export CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
 
+git clone git@github.com:debian12v23q3/24004-openos365-10024-debian12-docker-v23q3.git build
+cd build
+
+export GITHUB_REPOSITORY="openos365/24004-openos365-10024-debian12-docker-v23q3"
+
+./4.ci.build.run.sh
+
+
 date
